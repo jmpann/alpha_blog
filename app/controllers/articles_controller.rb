196b5a39718ba2ba227ef0123d0ee7f03ddaf0d5
Josh Pann <josh.pann@example.com>
2017-03-14 15:20:43 -0400

    show action complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,17 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.create(article_params)
-    redirect_to @article
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to @article
+    else
+      render :new
+    end
   end
 
   def show
-
+    @article = Article.find(params[:id])
   end
 
   private
