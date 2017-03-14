5391eb98f48137aaf3913d17fb110e6f30e9b3ce
Josh Pann <josh.pann@example.com>
2017-03-14 12:41:54 -0400

    article create action completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,22 @@
 class ArticlesController < ApplicationController
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    redirect_to @article
+  end
+
+  def show
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
