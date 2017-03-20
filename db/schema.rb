e323ee45c3b6f0979b6ba6b138e89f4b7a816896
Josh Pann <josh.pann@example.com>
2017-03-20 17:09:56 -0400

    created category model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318010114) do
+ActiveRecord::Schema.define(version: 20170320210923) do
 
   create_table "Posts", force: :cascade do |t|
     t.text     "title"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20170318010114) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "food"
+    t.string "movies"
+    t.string "music"
+    t.string "outdoors"
+    t.string "art"
   end
 
   create_table "users", force: :cascade do |t|
