f5f54ff5af804266c7a359595ac0e2946cb9d3b7
Josh Pann <josh.pann@example.com>
2017-03-21 17:23:23 -0400

    ready to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :users, except: [:new]
   resources :posts
+  resources :categories, only: [:show]
   # get 'posts/new', to: 'posts#new', as: "new_post"
   # post 'posts', to: 'posts#create'
   # get 'posts/:id', to: 'posts#show', as: "post"
