cb5c03955aa94a43fe18047105606a9e1ddb981b
Josh Pann <josh.pann@example.com>
2017-03-21 17:53:04 -0400

    installed postgres for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170321141445) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
   end
@@ -29,7 +32,7 @@ ActiveRecord::Schema.define(version: 20170321141445) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +45,5 @@ ActiveRecord::Schema.define(version: 20170321141445) do
     t.string   "last_name"
   end
 
+  add_foreign_key "posts", "users"
 end
